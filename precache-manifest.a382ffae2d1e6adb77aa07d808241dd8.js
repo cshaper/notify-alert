62587dbe459b9d9eef6483e19f4146c6718f6271
Priyanka <Priyanka.bhavsar45@hotmail.com>
2020-05-26 13:33:31 +0530

    Updates

---
+++ precache-manifest.a382ffae2d1e6adb77aa07d808241dd8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1af68f1d01c8177a1d2fa33666b7ece",
+    "url": "/react-notify-alert/index.html"
+  },
+  {
+    "revision": "34785019fe1dfe68c967",
+    "url": "/react-notify-alert/static/css/main.92ec89f8.chunk.css"
+  },
+  {
+    "revision": "ec12f7fdf06c665346e7",
+    "url": "/react-notify-alert/static/js/2.9655c4c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-notify-alert/static/js/2.9655c4c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34785019fe1dfe68c967",
+    "url": "/react-notify-alert/static/js/main.61135f26.chunk.js"
+  },
+  {
+    "revision": "9cf4bb10d1d38fbf1cd0",
+    "url": "/react-notify-alert/static/js/runtime-main.8a771bf3.js"
+  },
+  {
+    "revision": "5d20cf5c9b478459afa575d88054e2d2",
+    "url": "/react-notify-alert/static/media/confirmation-icon~bkEuhisw.5d20cf5c.png"
+  },
+  {
+    "revision": "f035af8bd0b3c09c810be596a678eed4",
+    "url": "/react-notify-alert/static/media/fail-icon~odjvrKNH.f035af8b.png"
+  },
+  {
+    "revision": "63e158e30a0ebb38fbb07b2bd9ffec7f",
+    "url": "/react-notify-alert/static/media/success-icon~IRfOBHXU.63e158e3.png"
+  }
+]);
